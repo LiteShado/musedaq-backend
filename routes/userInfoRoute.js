85e80016061223228acb2449341c19b1c3d3c72c
Natalie <theliteshado@icloud.com>
2021-05-23 22:44:08 -0700

    user route and controller

---
+++ routes/userInfoRoute.js
@@ -0,0 +1,12 @@
+const userInfoRoutes = require('express').Router()
+const userInfoController = require('../controllers/userController')
+
+
+userInfoRoutes.post('/', userInfoController.create)
+userInfoRoutes.post('/login', userInfoController.login)
+userInfoRoutes.get('/verify', userInfoController.verifyUser)
+userInfoRoutes.get('/label', userInfoController.getLabel)
+userInfoRoutes.delete('/label/:id', userInfoController.delete)
+
+
+module.exports = userInfoRoutes
